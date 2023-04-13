b349595ee608ea543a0add21f0e3e242369b5798
my coffee <pasega.wade@gmail.com>
2023-04-13 11:41:07 +0200

    change url

--- setup.py
+++ setup.py
@@ -7,24 +7,25 @@ build-backend = "setuptools.build_meta"
 """
 
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: ',
-  'Operating System :: Microsoft :: Windows :: Windows 11',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
 ]
- 
+
 setup(
-  name='text2embeddingview',
-  version='0.1.0',
-  description='A python package to map your own csv files data using Atlas from NOMIC',
-  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
-  url='https://github.com/papasega/text2embeddingview',  
-  author='Papa Séga WADE',
-  author_email='pasega.wade@gmail.com',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords='Embedding, Visualization, Map, Text, CSV,  Search keywords, dynamic', 
-  packages=find_packages(),
-  install_requires=['requirements.txt'] 
+    name='text2mapview',
+    version='0.2.0',
+    description='A python package to map your own csv files data using Atlas from NOMIC',
+    # + '\n\n' + open('CHANGELOG.md').read(),
+    long_description=open('README.md').read(),
+    url='https://github.com/papasega/text2mapview',
+    author='Papa Séga WADE',
+    author_email='pasega.wade@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Embedding, Visualization, Map, Text, CSV, Search keywords, Dynamic',
+    packages=find_packages(),
+    install_requires=['requirements.txt']
 )
